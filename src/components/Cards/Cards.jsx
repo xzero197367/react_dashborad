c74791ee10f024bed57a165232af426ff25ad61d
xzero197367 <xzero197367@gmail.com>
2022-12-10 17:30:01 +0200

    first commit

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,29 @@
+﻿import React from 'react'
+import './Cards.css'
+import Card from "../Card/Card";
+import {CardsData} from "../../Data/Data";
+
+const Cards = ()=>{
+    return(
+        <div className="Cards">
+            {
+                CardsData.map((card, id)=>{
+                    return(
+                        <div className="parentContainer">
+                            <Card
+                                title={card.title}
+                                color={card.color}
+                                barValue={card.barValue}
+                                value={card.value}
+                                png={card.png}
+                                series={card.series}
+                            />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Cards
